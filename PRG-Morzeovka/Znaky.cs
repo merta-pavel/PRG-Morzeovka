549dfbcafd35e503f5d2258191f1bbd441f80d42
Pavel Merta <pavel.merta.021@pslib.cz>
2023-01-11 10:25:34 +0100

    Add project files.

---
+++ PRG-Morzeovka/Znaky.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG_Morzeovka
+{
+    internal class Znaky
+    {
+        public Znaky(string latinka, string morzeovka) 
+        {
+            Latinka = latinka;
+            Morzeovka = morzeovka;
+        }
+        public string  Latinka { get; set; }
+        public string Morzeovka { get; set; }
+
+        private void
+        {
+
+        }
+    }
+}
